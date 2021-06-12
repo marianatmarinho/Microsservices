8e757658b7ea4f8985169088760a6d8ee72cb378
marianatmarinho <mariana.tmarinho@gmail.com>
2021-06-11 21:04:12 -0300

    versionando projeto de microsserviços

---
+++ Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,37 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if(!existProduct)
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetMyProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "602d2149e773f2a39990b47f5",
+                    Name = "Iphone X",
+                    Category = "Smartphone",
+                    Description = "celular inteligente",
+                    Image = "product1.png",
+                    Price = 3000
+                }
+            };
+        }
+    }
+}
